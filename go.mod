03c649fc9d0d2703944fde7b249ca2ac3289390c
Paul <70671169+PaulSchiretz@users.noreply.github.com>
2026-02-05 15:32:32 +0100

    updated dependencies

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/paulschiretz/pgl-backup
 go 1.25
 
 require (
-	github.com/klauspost/compress v1.18.2
+	github.com/klauspost/compress v1.18.3
 	github.com/klauspost/pgzip v1.2.6
-	golang.org/x/sys v0.38.0
+	golang.org/x/sys v0.40.0
 )
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -194,3 +194,17 @@ func GenerateUUID() (string, error) {
 
 	return string(buf), nil
 }
+
+// ByteCountIEC converts bytes to human-readable string using IEC standard (KiB, MiB, GiB).
+func ByteCountIEC(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB", float64(b)/float64(div), "KMGTPE"[exp])
+}
